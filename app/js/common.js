dfa07383093bd1a355b24fa1f9337ce6a9afb6c9
Slowfind <danilkoguhov@yandex.ru>
2019-03-30 23:09:24 +0400

    fix 2

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-	 
+	
   $(".slider-carousel").owlCarousel({
   		loop: true,
   		nav: true,
   		items: 3,
   		center: true,
   		smartSpeed: 700,
-  		navText: ['<i id="id2" class="fas fa-long-arrow-alt-left"></i>', '<i id="id1" class="fas fa-long-arrow-alt-right"></i>'],
+  		navText: ['<i class="fas fa-long-arrow-alt-left"></i>', '<i class="fas fa-long-arrow-alt-right"></i>'],
   		responsiveClass: true,
   		responsive: {
   			0: {
